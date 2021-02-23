5414d00ad56c3ee0ee935a4e63b35b939767dac9
Slobodanka Tešmanović <st20193711@student.fon.bg.ac.rs>
2021-02-23 18:39:31 +0100

    #2 add main function

--- project/src/project/core.clj
+++ project/src/project/core.clj
@@ -1,6 +1,31 @@
-(ns project.core)
+(ns project.core
+  (:gen-class)
+  (:require [clj-http.client :as client])
+  (:require [clojure.string])
+  (:require [clojure.java.io])
+  (:require [clojure.data.xml :as xml]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn read-rss-config-file []
+  (with-open [reader (clojure.java.io/reader "config.txt")]
+    (reduce conj [] (line-seq reader))))
+
+(def help-text "Usage :[command]
+ commands:
+  :h - help
+  :lr - list feeds
+  :ls - list stories
+  :s[num] - number of listed storie
+  :r[num] - set current feed
+  :q - quit")
+
+(defn -main
+  "Main function"
+  [& args]
+  (println help-text)
+  (println "Enter command: ")
+
+  (loop [feed ""
+         input (read-line)]
+  )
+
+  (println "End"))
