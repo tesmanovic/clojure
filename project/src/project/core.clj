fd66589a7c331e94c13acd5c374f41b19ad688a4
Slobodanka Tešmanović <st20193711@student.fon.bg.ac.rs>
2021-03-17 16:22:42 +0100

    #7 add fns

--- project/src/project/core.clj
+++ project/src/project/core.clj
@@ -8,9 +8,11 @@
 (defn read-rss-config-file []
   (with-open [reader (clojure.java.io/reader "config.txt")]
     (reduce conj [] (line-seq reader))))
+
 (defn get-feed [url]
   (client/get url
               {:accept :xml}))
+
 (defn transform-to-xml [xml]
   (let [input-xml (java.io.StringReader. xml)]
     (xml/parse input-xml)))
@@ -34,6 +36,21 @@
 
 (defn numbering [col]
   (map (fn [text num] (str num ". " text)  ) col (range (count col))))
+
+(defn formated-titles [feed]
+  (->> feed
+       (extract-channel)
+       (extract-items)
+       (extract-titles)
+       (map (fn [title] (first (:content title))))
+       (numbering)))
+
+(defn formated-rss []
+  (->> (read-rss-config-file)
+       (numbering)))
+
+(defn nice-print [col]
+  (doseq [item col] (println item)))
 
 (defn choosen-item [feed num]
   (->  feed
@@ -73,7 +90,9 @@
       (recur
         (cond
           (not= (parse-command-number "r" input) "nil") (get-feed (nth (read-rss-config-file) (Integer/parseInt (parse-command-number "r" input))))
+          (= ":lr" input) (do (nice-print (formated-rss)) feed)
           (= feed "") (do (println "Need feed") feed)
+          (= ":ls" input) (do (nice-print (formated-titles feed)) feed)
           (= ":h" input) (do (println help-text) feed)
           (not= (parse-command-number "s" input) "nil") (do (println (description feed (Integer/parseInt (parse-command-number "s" input)))) feed)
           :else (do (println "Uknown command") (println help-text) feed))
